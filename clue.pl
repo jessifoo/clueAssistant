272d03b78a4837fa4cff05b84b6bb9970992d0d0
Anthony Nixon <anixon604@gmail.com>
2012-11-23 17:53:14 -0800

    Changed Card Entry and ShownCard
    entercard now takes the player number as well.
    
    shownCard is now arrity 3 (player,card,probability)

--- clue.pl
+++ clue.pl
@@ -2,21 +2,21 @@
 Dynamic predicates to represent suspected weapons, persons, and rooms
 */
 
-% Current Suspects
+% Current Suspects suspect(id,eval)
 :- dynamic suspect/2.
 
-% Current Possible Weapons
+% Current Possible Weapons mweapon(id,eval)
 :- dynamic mweapon/2.
 
-% Current Possible Rooms
+% Current Possible Rooms mroom(id,eval)
 :- dynamic mroom/2.
 
-% Current player room
+% Room player is currently in (no room = corridor)
 :- dynamic playerRoom/1.
 
-% List of all cards which have been shown, shown cards are eliminated from solution.
-% format: shownCard(player,Card)
-:- dynamic shownCard/2.
+% List of all cards which have been shown or held up, shown cards are assigned prob of 100% and eliminated from solution. An unknown card which is held up is assigned a probability that the player has that card.
+% shownCard(player,Card,probability)
+:- dynamic shownCard/3.
 
 % Number of cards each player has.
 :- dynamic numPlayerCards/1.
@@ -86,19 +86,19 @@ assert(numPlayers(Players)),
 write('Enter the number of cards you recieved: '),
 read(Numcards),
 assert(numPlayerCards(Numcards)),
-entercards(Numcards),
+entercards(1,Numcards),
 playLoop.
 
 % procedure to enter cards into play
-entercards(0).
-entercards(N) :-
+entercards(_,0).
+entercards(P,N) :-
 N>0, % make sure positive number
 write('Enter your first/next card: '),
 read(Card),
 isValidCard(Card),
-assert(shownCard(1,Card)),
+assert(shownCard(P,Card,100)),
 M is N-1,
-entercards(M).
+entercards(P,M).
 
 % removes a card that has been entered from possible solutions.
 removeFromPlay(X) :- isWeapon(X), retract(mweapon(X)).
@@ -132,9 +132,9 @@ min(X) :- mroom(X,Z),not((mroom(X,Other),Other<Z)),!.
 executeOption(1) :-
 nl,
 write('A good guess is: '),
-suspect(X,_),min(X),not(shownCard(_,X)),
-mweapon(Y,_),min(Y),not(shownCard(_,Y)),
-mroom(Z,_),min(Z),not(shownCard(_,Z)),
+suspect(X,_),min(X),not(shownCard(_,X,_)),
+mweapon(Y,_),min(Y),not(shownCard(_,Y,_)),
+mroom(Z,_),min(Z),not(shownCard(_,Z,_)),
 write(X),
 write(' with the '),
 write(Y),
@@ -146,7 +146,9 @@ showOptions.
 
 executeOption(2) :-
 nl,
-entercards(1),
+write('Which Player showed you the card (number)? '),
+read(Player),
+entercards(Player,1),
 nl,
 showOptions.
 
@@ -158,43 +160,101 @@ write('You are currently in the '),
 (playerRoom(X) -> write(X),
 write('. Leave room [Y/N]? '),
 read(Ans),
-((Ans = 'Y' ; Ans = 'y') -> retract(playerRoom(_)),! ; true), showOptions ;
+((Ans = 'Y' ; Ans = 'y') -> retract(playerRoom(_)) ; true) ;
 write('corridor. Which room would you like to enter? '),
 read(Room),
-assert(playerRoom(Room))),
+assert(playerRoom(Room))), % end if-then-else
 showOptions.
 
 % PUT CODE TO PROCESS AN OPPONENTS GUESS HERE ////
 executeOption(4) :-
 nl,
+write('Which opponent made the guess (number)? '),
+read(Player),
 write('Enter your opponents guess in the form [person,weapon,room] : '),
 read(GuessArray),
-opponentGuess(GuessArray),
+opponentGuess(GuessArray,Player),
 showOptions.
 
 % show remaining cards again
 executeOption(5) :- printAvailCards.
 
-executeOption(6) :- retractall(shownCard(_,_)), retractall(numPlayerCards(_)), retractall(numPlayers(_)), retractall(playerRoom(_)),
-    retractall(suspect(_,_)), retractall(mweapon(_,_)),
-    retractall(mroom(_,_)), false.
+executeOption(6) :- clear.
 
+clear :- retractall(shownCard(_,_,_)), retractall(numPlayerCards(_)), retractall(numPlayers(_)), retractall(playerRoom(_)),
+    retractall(suspect(_,_)), retractall(mweapon(_,_)),
+    retractall(mroom(_,_)),retractall(guessCards(_)), false.
+
+%check its not out of play and then assign card to player
+opponentGuess([H|T],P) :-
+(not(shownCard(_,H,100)) -> assert(guessCards(H,0)) ; true),
+((shownCard(P,H,100)) -> assert(guessCards(H,1)) ; true),
+opponentGuess(T,P).
+opponentGuess([],P) :- assignCards(P), showOptions.
+
+% holds the valid cards from opponent guess
+% guessCards(ID,0 = unowned card,1 = owned by THIS opponent)
+:- dynamic guessCards/2.
+
+
+%case 1: no other opponent has any of the three cards
+%ALL three possible cards are added with a probabilty of 30
+
+assignCards(P) :- guessCards(X,0),guessCards(Y,0),guessCards(Z,0),
+X \= Y, Y \= Z, X \= Z,
+assert(shownCard(P,X,30)),assert(shownCard(P,Y,30)),assert(shownCard(P,Z,30)),!.
+
+%case 2: one of the cards is held by an opponent != P w/ prob 100
+%two unheld cards are added with a probability of 50
+
+assignCards(P) :- guessCards(X,0),guessCards(Y,0),
+X \= Y,
+assert(shownCard(P,X,50)),assert(shownCard(P,Y,50)),!.
+
+%case 2b: one of the cards is held by this opponent == P w/ prob 100
+
+assignCards(P) :- guessCards(X,0),guessCards(Y,0),guessCards(Z,1),
+X \= Y, Y \= Z, X \= Z,
+assert(shownCard(P,X,30)),assert(shownCard(P,Y,30)),!.
+
+%case 3: two of the cards are held by opponent != P w/ prob 100
+%unheld card is added to player w/ prob 100
+
+assignCards(P) :- guessCards(X),
+assert(shownCard(P,X,100)),!.
+
+%case 3b: two of the cards are held by this opponent == P w/ prob 100
+
+assignCards(P) :- guessCards(X,0),guessCards(Y,1),guessCards(Z,1),
+X \= Y, Y \= Z, X \= Z,
+assert(shownCard(P,X,30)),!.
+
+%case 4: cards are held by other players with a percentage < 100
+%each card (A,B,C) is assigned 30% * (100%-percentage of card being held by others)
+
+
+assignCards(_).
+
+adder(X,Y,Z) :- Z is X+Y.
+
+/*
 % HELPER for executeOption(4) to process an opponents guess
 % modify suspect entry
 opponentGuess([H|T]) :-
 isPerson(H),
-(not(shownCard(_,H)) -> retract(suspect(H,Y)),
+(not(shownCard(_,H,_)) -> retract(suspect(H,Y)),
 incr(Y,Y1), assert(suspect(H,Y1)) ; true),
 opponentGuess(T).
 opponentGuess([H|T]) :-
 isWeapon(H),
-(not(shownCard(_,H)) -> retract(mweapon(H,Y)),
+(not(shownCard(_,H,_)) -> retract(mweapon(H,Y)),
 incr(Y,Y1),assert(mweapon(H,Y1)) ; true),
 opponentGuess(T).
 opponentGuess([H]) :-
 isRoom(H),
-(not(shownCard(_,H)) -> retract(mroom(H,Y)),
+(not(shownCard(_,H,_)) -> retract(mroom(H,Y)),
 incr(Y,Y1),assert(mroom(H,Y1)) ; true).
+*/
 
 % number incrementer
 incr(X,X1) :- X1 is X+1.
@@ -207,7 +267,7 @@ printSuspects :-
 nl,
 write('Current Suspects: '),nl,nl,
 suspect(X,_),
-not(shownCard(1,X)),
+not(shownCard(_,X,100)),
 write(X),
 nl,
 fail.
@@ -216,7 +276,7 @@ printWeapons :-
 nl,
 write('Possible Weapons: '),nl,nl,
 mweapon(X,_),
-not(shownCard(1,X)),
+not(shownCard(_,X,100)),
 write(X),
 nl,
 fail.
@@ -225,22 +285,23 @@ printRooms :-
 nl,
 write('Possible Rooms: '),nl,nl,
 mroom(X,_),
-not(shownCard(1,X)),
+not(shownCard(_,X,100)),
 write(X),
 nl,
 fail.
 
-% clean database of cards
-clean :- abolish(shownCard/2).
 
 /* NOTES and ideas
 
 1. Anthony
 
 make a dynamic predicate called
-opponentsCards(player,probability)
+opponentsCards(player,card,probability)
+
+ADD all YOUR cards with prob 100
 
 when a player shows YOU a card during your turn it is added with probability of 100
+
 when a player holds up a card to beat the guess of another player
 
 case 1: no other opponent has any of the three cards
@@ -249,10 +310,14 @@ ALL three possible cards are added with a probabilty of 30
 case 2: one of the cards is held by another opponent w/ prob 100
 two unheld cards are added with a probability of 50
 
-case 3: two of the cards is w/ prob 100
+case 3: two of the cards are held by opponents w/ prob 100
 unheld card is added to player w/ prob 100
 
-case 4: 
+case 4: cards are held by other players with a percentage < 100
+each card (A,B,C) is assigned 30% * (100%-percentage of card being held by others)
+
+
+
 
 2. Jessyka
 
